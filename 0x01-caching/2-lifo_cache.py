d289cce8f089cc1a2662504c77e7b8a48ddc4acf
root <Jusca.mol@gmail.comgit config --global user.name Jusca Molaiwa>
2024-10-30 14:02:36 +0000

    caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python3
-"""Last-In First-Out (LIFO) caching strategy.
+"""Last-In First-Out caching module.
 """
 from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Defines a caching system that stores items,
-    with the Last-In, First-Out (LIFO) removal
-    policy when the cache limit is exceeded.
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
     """
-    
     def __init__(self):
-        """Sets up the LIFO cache instance."""
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Inserts an item into the cache.
-        
-        If adding the new item causes the cache to exceed
-        the predefined size limit, the most recently added
-        item is removed.
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
-
-        # Remove the most recent entry if size limit is reached
-        if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key, _ = self.cache_data.popitem(last=True)
-            print("DISCARD:", last_key)
-        
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Fetches an item by its key from the cache.
-        
-        Returns None if the key is not present.
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
-
